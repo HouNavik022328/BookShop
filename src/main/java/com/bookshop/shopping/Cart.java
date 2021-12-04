8b5c7a09b90cca3529a3df7c063ef5be795ac6c4
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 13:36:55 +0700

    Update Cart.java

--- src/main/java/com/bookshop/shopping/Cart.java
+++ src/main/java/com/bookshop/shopping/Cart.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bookshop.shopping;
 
 import java.util.HashMap;
 import java.util.Map;
-
-/**
- *
- * @author VODUCMINH
- */
 public class Cart {
     private Map<String, CartProduct> cart;
 
