67371aba8d7d159fb442704dcdb2307ccf7f6542
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 15:11:16 +0700

    Update LogoutController.java

--- src/main/java/com/bookshop/controller/LogoutController.java
+++ src/main/java/com/bookshop/controller/LogoutController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bookshop.controller;
 
 import java.io.IOException;
@@ -13,11 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-/**
- *
- * @author VODUCMINH
- */
 @WebServlet(name = "LogoutController", urlPatterns = {"/LogoutController"})
 public class LogoutController extends HttpServlet {
     private static final String ERROR = "login.html";
