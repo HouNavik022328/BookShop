df8e1dca5909c31264087fef4fbefca7262f6dd8
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 15:13:37 +0700

    Update VNPayCheckoutController.java

--- src/main/java/com/bookshop/controller/VNPayCheckoutController.java
+++ src/main/java/com/bookshop/controller/VNPayCheckoutController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bookshop.controller;
 
 import com.bookshop.orders.Order;
@@ -26,11 +21,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-/**
- *
- * @author VODUCMINH
- */
 @WebServlet(name = "VNPayCheckoutController", urlPatterns = {"/VNPayCheckoutController"})
 public class VNPayCheckoutController extends HttpServlet {
     private static final String ERROR = "checkout.jsp";
