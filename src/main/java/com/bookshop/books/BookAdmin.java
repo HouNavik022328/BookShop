ab92c8eea7001c25c693c5216772f8d9365385ce
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 13:32:51 +0700

    Update BookAdmin.java

--- src/main/java/com/bookshop/books/BookAdmin.java
+++ src/main/java/com/bookshop/books/BookAdmin.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bookshop.books;
-
-/**
- *
- * @author VODUCMINH
- */
 public class BookAdmin {
     private String ID;
     private String name;
