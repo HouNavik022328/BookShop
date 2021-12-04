f077170fe140075beeab152996ba0199173c35e2
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 15:04:34 +0700

    Update DeleteCartController.java

--- src/main/java/com/bookshop/controller/DeleteCartController.java
+++ src/main/java/com/bookshop/controller/DeleteCartController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.bookshop.controller;
 
 import com.bookshop.shopping.Cart;
@@ -14,11 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-/**
- *
- * @author VODUCMINH
- */
 @WebServlet(name = "DeleteCartController", urlPatterns = {"/DeleteCartController"})
 public class DeleteCartController extends HttpServlet {
     private static final String ERROR = "cart.jsp";
