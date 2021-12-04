9ba67c72cb6d13498388f81a739339cb37156ec3
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 13:33:42 +0700

    Update BookDTO.java

--- src/main/java/com/bookshop/books/BookDTO.java
+++ src/main/java/com/bookshop/books/BookDTO.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bookshop.books;
 
-/**
- *
- * @author VODUCMINH
- */
 public class BookDTO {
     private String ID;
     private String name;
