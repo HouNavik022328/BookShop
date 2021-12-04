254d05b5b0474641326976f5c809bb634eda826f
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 13:33:59 +0700

    Update Category.java

--- src/main/java/com/bookshop/books/Category.java
+++ src/main/java/com/bookshop/books/Category.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.bookshop.books;
 
-/**
- *
- * @author VODUCMINH
- */
 public class Category {
     private int ID;
     private String name;
