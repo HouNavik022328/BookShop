6175b0ade0a9c8c4f5c49e02a0ede99bd8258ede
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 13:38:10 +0700

    Update DBUtils.java

--- src/main/java/com/bookshop/utils/DBUtils.java
+++ src/main/java/com/bookshop/utils/DBUtils.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.bookshop.utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-/**
- *
- * @author VODUCMINH
- */
 public class DBUtils {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         final String url = "jdbc:mysql://localhost:3306/bookshop";
