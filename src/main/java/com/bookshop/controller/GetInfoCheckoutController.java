eb2ebdae55ab7817ccea08bd2a0e9eafc3cfc508
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 15:07:27 +0700

    Update GetInfoCheckoutController.java

--- src/main/java/com/bookshop/controller/GetInfoCheckoutController.java
+++ src/main/java/com/bookshop/controller/GetInfoCheckoutController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bookshop.controller;
 
 import com.bookshop.orders.ShippingInfo;
@@ -20,11 +15,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-/**
- *
- * @author VODUCMINH
- */
 @WebServlet(name = "GetInfoCheckoutController", urlPatterns = {"/GetInfoCheckoutController"})
 public class GetInfoCheckoutController extends HttpServlet {
     private static final String ERROR = "checkout.jsp";
