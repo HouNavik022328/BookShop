0f10237f0160e37e80fc391b4afbd7b3b8e9ea35
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 15:12:24 +0700

    Update MainController.java

--- src/main/java/com/bookshop/controller/MainController.java
+++ src/main/java/com/bookshop/controller/MainController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.bookshop.controller;
 
 import java.io.IOException;
@@ -12,11 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-/**
- *
- * @author VODUCMINH
- */
 @WebServlet(name = "MainController", urlPatterns = {"/MainController"})
 public class MainController extends HttpServlet {
     private static final String ERROR = "error.jsp";
