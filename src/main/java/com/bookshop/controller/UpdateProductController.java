13914e8f64b78eca35dc4a3a355e80b177eb3e34
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 15:13:06 +0700

    Update UpdateProductController.java

--- src/main/java/com/bookshop/controller/UpdateProductController.java
+++ src/main/java/com/bookshop/controller/UpdateProductController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.bookshop.controller;
 
 import com.bookshop.books.BookDAO;
@@ -13,11 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author VODUCMINH
- */
 @WebServlet(name = "UpdateProductController", urlPatterns = {"/UpdateProductController"})
 public class UpdateProductController extends HttpServlet {
     private static final String ERROR = "admin-product.jsp";
