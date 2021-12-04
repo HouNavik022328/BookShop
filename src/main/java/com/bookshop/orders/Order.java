5347cf84d56d4d68ca9248fc7f224e6cff88fd16
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 13:35:09 +0700

    Update Order.java

--- src/main/java/com/bookshop/orders/Order.java
+++ src/main/java/com/bookshop/orders/Order.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.bookshop.orders;
 
-/**
- *
- * @author VODUCMINH
- */
+package com.bookshop.orders;
 public class Order {
     private String orderID;
     private String userID;
