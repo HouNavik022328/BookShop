fe96db014829179f24bbc08a0e363944b9cc1df6
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 15:05:58 +0700

    Update GetCateController.java

--- src/main/java/com/bookshop/controller/GetCateController.java
+++ src/main/java/com/bookshop/controller/GetCateController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.bookshop.controller;
 
 import com.bookshop.books.BookDAO;
@@ -16,11 +12,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author VODUCMINH
- */
 @WebServlet(name = "GetCateController", urlPatterns = {"/GetCateController"})
 public class GetCateController extends HttpServlet {
     private static final String ERROR = "category.jsp";
