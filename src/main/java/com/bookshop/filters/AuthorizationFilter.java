0571978afe6f9042b4e9036cfd7945680643acc2
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 15:14:44 +0700

    Update AuthorizationFilter.java

--- src/main/java/com/bookshop/filters/AuthorizationFilter.java
+++ src/main/java/com/bookshop/filters/AuthorizationFilter.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bookshop.filters;
 
 import com.bookshop.users.UserDTO;
@@ -19,12 +14,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-
-/**
- *
- * @author VODUCMINH
- */
 public class AuthorizationFilter implements Filter {
     
     private static final boolean debug = true;
