cfeb8fe622487a3d424afdcd79891fd8c179c667
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 13:37:44 +0700

    Update UserDTO.java

--- src/main/java/com/bookshop/users/UserDTO.java
+++ src/main/java/com/bookshop/users/UserDTO.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.bookshop.users;
 
-/**
- *
- * @author VODUCMINH
- */
+package com.bookshop.users;
 public class UserDTO {
     private String userID;
     private String userName;
