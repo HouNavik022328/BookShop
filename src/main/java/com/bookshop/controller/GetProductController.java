fb20928506e5ec13420507c235db29cec30884ef
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 15:10:15 +0700

    Update GetProductController.java

--- src/main/java/com/bookshop/controller/GetProductController.java
+++ src/main/java/com/bookshop/controller/GetProductController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.bookshop.controller;
 
 import com.bookshop.books.BookAdmin;
@@ -16,11 +12,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author VODUCMINH
- */
 @WebServlet(name = "GetProductController", urlPatterns = {"/GetProductController"})
 public class GetProductController extends HttpServlet {
 
