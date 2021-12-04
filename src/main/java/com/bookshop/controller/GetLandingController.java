b0ad1574de2dd54ae7ced9c6b82595d2708fd6de
HouNavik <69951094+HouNavik022328@users.noreply.github.com>
2021-12-04 15:07:53 +0700

    Update GetLandingController.java

--- src/main/java/com/bookshop/controller/GetLandingController.java
+++ src/main/java/com/bookshop/controller/GetLandingController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bookshop.controller;
 
 import com.bookshop.books.BookDAO;
@@ -17,11 +12,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author VODUCMINH
- */
 @WebServlet(name = "GetLandingController", urlPatterns = {"/GetLandingController"})
 public class GetLandingController extends HttpServlet {
 
